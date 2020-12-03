b29665db759f75f2e23b9f807ccb9781ba33d85a
Fernanda <fernanda-herrera@hotmail.com>
2020-12-03 13:14:48 +0100

    test

---
+++ script.js
@@ -0,0 +1,71 @@
+function formatPhone() {
+  var phone_number = document.getElementById('phone').value;
+  if (phone_number.length === 10) {
+    phone_number = phone_number.replace(/[^0-9]/g, '');
+    phone_number = phone_number.replace(/(\d{3})(\d{3})(\d{4})/, "($1) $2-$3");
+
+
+    document.getElementById('formatted-phone').innerHTML = "+1" + phone_number;
+  }
+  else {
+    document.getElementById('formatted-phone').innerHTML = phone_number;
+  }
+}
+
+var s = document.getElementById('phone');
+
+s.addEventListener('input', formatPhone);
+s.addEventListener('propertychange', formatPhone);
+
+
+
+
+
+
+
+//switch darkmode lightmode;
+document.querySelector('[data-switch-dark]').addEventListener('click', function () {
+  document.body.classList.toggle('dark');
+});
+
+
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+  }
+}
+
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+  }
+  else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+  }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+
+
+onclick => changeColorButton();
+
+function changeColorButton() {
+
+  document.getElementById('#"Home, Host, HostConfig"').style.BackgroundColor = "#10D6E6";
+
+}
+
+document.addEventListener('click', event => {
+  if (event.target.matches('btn-menu')) {
+    event.target.focus()
+  }
+})
